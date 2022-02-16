7a3bd4f71f13072ae9e10acce17527bbe7a3cec9
Skruphold <hunterco999@gmail.com>
2022-02-16 14:35:30 -0600

    added some more routes for the user all are working!

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,6 +16,34 @@ const userController = {
             res.status(500).json(err);
         });
     },
+
+    getUsersById({params}, res) {
+        User.findOne({_id: params.id })
+        .select('-__v')
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No User with this particular ID!'});
+                return; 
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    },
+
+    updateUsers({params, body}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No User with this particular ID!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err))
+    },
 }
 
 module.exports = userController; 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 
 const {
+    createUsers,
     getAllUsers,
-    createUsers
+    getUsersById,
+    updateUsers,
 } = require('../../controllers/user-controller');
 
 router.route('/').get(getAllUsers).post(createUsers);
+router.route('/:id').get(getUsersById).put(updateUsers)
 
 module.exports = router; 

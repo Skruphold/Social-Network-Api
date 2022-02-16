c3bb5b066bf5eb43b16bd82eaeb8e71871f4931d
Skruphold <hunterco999@gmail.com>
2022-02-15 20:22:15 -0600

    added server file got it running installed packages! added file structure

---
+++ server.js
@@ -0,0 +1,18 @@
+// Require express and mongoose
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(require('routes'));
+
+mongoose.connect('mongodb://localhost/social-network-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.listen(PORT, () => console.log(` Connected on localhost:${PORT} `));

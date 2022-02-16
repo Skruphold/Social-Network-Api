5c70a3f31c727c64a98eef8af881f332a972438c
Skruphold <hunterco999@gmail.com>
2022-02-15 21:15:37 -0600

    added models and also a seeds for some data

---
+++ utils/seed.js
@@ -0,0 +1,15 @@
+const { User, Thought } = require('../models');
+
+User.deleteMany({});
+Thought.deleteMany({});
+
+const users = [{ username: "hunter", email: "hunter@test.com", thoughts: [], friends: [] }, { username: "hunter2", email: "hunter2@test.com", thoughts: [], friends: [] }, { username: "hunter3", email: "hunter3@test.com" }];
+const thoughts = [{ username: "hunter1", thoughtText: "1st hunter" }, { username: "hunter2", thoughtText: "second hunter" }, { username: "hunter3", thoughtText: "third hunter" }]
+
+User.collection.insertMany(users);
+Thought.collection.insertMany(thoughts);
+
+console.table(users);
+console.table(thoughts);
+console.info('Seeding complete! 🌱');
+process.exit(0);

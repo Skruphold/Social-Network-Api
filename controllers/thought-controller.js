a2181536045d91a33267a31838ed4e366d543774
Skruphold <hunterco999@gmail.com>
2022-02-16 19:30:35 -0600

    added more thought routes all are working.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,9 +1,7 @@
 const {Thought, User} = require('../models');
 
-// Set up Thoughts Controller
 const thoughtController = {
 
-    // Create a new thought
     createThoughts({params, body}, res) {
         Thought.create(body)
         .then(({_id}) => {
@@ -28,7 +26,47 @@ const thoughtController = {
             res.status(500).json(err);
         });
     },
+
+    getThoughtsById({params}, res) {
+        Thought.findOne({ _id: params.id })
+        .select('-__v')
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+            res.status(404).json({message: 'No thoughts with this particular ID!'});
+            return;
+        }
+        res.json(dbThoughtData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    updateThoughts({params, body}, res) {
+        Thought.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .select('-___v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({message: 'No thoughts with this particular ID!'});
+                return;
+            }
+                res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    deleteThoughts({params}, res) {
+        Thought.findOneAndDelete({_id: params.id})
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({message: 'No thoughts with this particular ID!'});
+                return;
+            }
+            res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 };
 
-// Export module thought controller
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,10 +2,14 @@ const router = require('express').Router();
 
 const { 
     createThoughts,
-    getAllThoughts, 
+    getAllThoughts,
+    getThoughtsById,
+    updateThoughts,
+    deleteThoughts,
 } = require('../../controllers/thought-controller');
 
 router.route('/:userId').post(createThoughts);
 router.route('/').get(getAllThoughts);
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts);
 
 module.exports = router;
